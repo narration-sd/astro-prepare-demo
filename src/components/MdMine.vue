fe69be3e68beb95bd28afb656ce2498f4ea2a581
Narration SD <narrationsd@gmail.com>
2022-04-23 04:09:53 -0700

    Commit for working Two, which includes a Vue3 component presenting npm components. A number of things discovered, so bears close scrutiny. In particular, but hardly alone, the ={variable} params. And that you can't use setup scripts, again it seems. Next destination, netlify, and porting in the Fauna data.

---
+++ src/components/MdMine.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>Here from MD</h2>
+    <MD :source="source" breaks=true />
+    <h2>Datapicker</h2>
+    <Datepicker v-model="picked" />
+  </div>
+</template>
+<script setup>
+// note we can't even import here, much less set up children - out of order somehow
+</script>
+
+<script>
+import MD from 'vue3-markdown-it'
+import Datepicker from 'vue3-datepicker'
+import { ref } from 'vue'
+// const picked = ref(new Date())
+
+export default {
+  name: 'MdMine',
+  props: {
+    source: {
+      type: String,
+      default: 'no source prop yet!'
+    },
+  },
+  data: function () {
+    return {
+      picked: ref(new Date())
+    }
+  },
+  created() {
+    console.log('source prop is: '+ this.source)
+  },
+  components: { MD, Datepicker }
+}
+</script>
---
+++ src/content/Two.md
@@ -0,0 +1,13 @@
+## Here we have Two
+
+### We could have some better Markdown
+
+- for example,
+  - bullets
+  - in an 
+  - indented list
+- non-indent items also
+
+## 👀 Want to learn more?
+
+Check out the [Astro Documentation](https://github.com/withastro/astro) site or jump into our [Discord server](https://astro.build/chat).
