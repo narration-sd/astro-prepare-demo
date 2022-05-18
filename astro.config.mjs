5e82e1ba8bec735e33c87fb5a86ce7b0d5fa96d0
Narration SD <narrationsd@gmail.com>
2022-05-18 22:45:07 +0000

    pull out the .idea from git  on gitpod??

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 // import md from 'integrations/md'
-import turbolinks from "@astrojs/turbolinks";
+// import turbolinks from "@astrojs/turbolinks";
 
 // https://astro.build/config
 export default defineConfig({
   // integrations: [vue(), turbolinks(), md()]
-  integrations: [vue(), turbolinks()]
+  // integrations: [vue(), turbolinks()]
+  integrations: [vue()]
   // integrations: [vue()]
 });
