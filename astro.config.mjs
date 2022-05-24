3a2a181d779baaf6cf7bf18561c848704ec6de1b
nsd <narrationsd@gmail.com>
2022-05-23 20:37:20 -0700

    somehow in merge didn't complete taking turbolinks out of config - this anyway is the Swup version of life...

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vue from '@astrojs/vue';
 // https://astro.build/config
 export default defineConfig({
   // integrations: [vue(), turbolinks(), md()]
-  integrations: [vue(), turbolinks()]
-  // integrations: [vue()]
+  // integrations: [vue(), turbolinks()]
+  integrations: [vue()]
 });
