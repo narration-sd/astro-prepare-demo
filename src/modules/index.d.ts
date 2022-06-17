e9f0098fab87256a8a0aa2971e65c4d3c5c5cfe9
nsd <narrationsd@gmail.com>
2022-06-17 02:34:31 -0700

    add duplication prevention, and try the Head plugin to get Swup to run the css as well as the scripts. Except this hard crashes, so commented out in this commit, to be investigated after.

--- src/modules/index.d.ts
+++ src/modules/index.d.ts
@@ -1,3 +1,4 @@
 declare module "swup"
 declare module "@swup/scripts-plugin"
+declare module "@swup/head-plugin"
 declare module "@swup/debug-plugin"
--- src/modules/swup.ts
+++ src/modules/swup.ts
@@ -1,15 +1,22 @@
 import Swup from 'swup'
 import SwupScriptsPlugin from '@swup/scripts-plugin';
+import SwupHeadPlugin from '@swup/head-plugin';
 import SwupDebugPlugin from '@swup/debug-plugin';
 
-const swup = new Swup({
-    // debugMode: true,
-    containers: [ /* "#app-top",*/ /* "#myswup", */  "#navslot" ],
-    plugins: [
-        new SwupDebugPlugin(),
-        new SwupScriptsPlugin(  {
-        	head: true, // hmm
-        	body: true
-        } )
-    ]
-})
+let swup = null
+if (/*false && */typeof window !== "undefined" && typeof window.swup === "undefined") {
+    swup = new Swup({
+        // debugMode: true,
+        containers: [ /* "#app-top",*/ /* "#myswup", */  "#navslot" ],
+        plugins: [
+            new SwupDebugPlugin(),
+            // new SwupHeadPlugin(),
+            new SwupScriptsPlugin(
+                {
+                // head: true, // hmm
+                // body: true
+                }
+            )
+        ]
+    })
+}
