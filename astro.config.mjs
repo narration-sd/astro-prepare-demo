c9cf870097b0d5c32f40af4ee861ba1df590d149
nsd <narrationsd@gmail.com>
2022-07-20 03:00:10 -0700

    astro.config here is all set to pass an array of clientPlugins, starting with pinia and persist, whose best way works only if I add the type:module-quoted json into the persist's package.json. This could be fixed with a fork or pull, but in any case  it's not going to work as today's astro-nsd explores, finding we're stuck with strings, not arrays or even functions. My sense at this moment is that this is headed for a package=patch on the astro integration side.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,75 @@ import vue from '@astrojs/vue';
 // import md from 'integrations/md'
 // import turbolinks from "@astrojs/turbolinks";
 
-import { createPinia } from 'pinia'
+// this first will work, but w/o persistence
+// import { createPinia } from 'pinia'
 // import { default as persistPinia } from 'pinia-plugin-persist'
 // nuts
 // const persistPinia = require('pinia-plugin-persist')
 // nuts
 // const persistPinia = import ('pinia-plugin-persist')
 
+// and so, no go...
+// const preparePinia = function (vueApp) {
+//   try {
+//     const pinia = createPinia()
+//     // pinia.use(persistPinia )
+//     console.log('PINIA SCRIPT-CREATED: ' + JSON.stringify(pinia))
+//     vueApp.use(pinia)
+//   }
+//   catch (err) {
+//     console.error ('preparePinia: ' + err)
+//   }
+// }
 
-const preparePinia = function (vueApp) {
-  try {
-    const pinia = createPinia()
-    // pinia.use(persistPinia )
-    console.log('PINIA SCRIPT-CREATED: ' + JSON.stringify(pinia))
-    vueApp.use(pinia)
-  }
-  catch (err) {
-    console.error ('preparePinia: ' + err)
-  }
+// here we try functional import to see if it works at right time & location
+// doesn't work first time for same reason, but have we an opportunity to use
+// a fix based also on promise chaining?
+// also basis ok, but doesn't prove yet for actuation later from client.mjs
+
+
+// import { createPinia } from 'pinia'
+// const { createPinia } = import('pinia')
+// const piniaPersist = 99 // { piniaPersist } = import ('pinia-plugin-persist')
+console.log('runnning config')
+const preparePinia = function (app) {
+    console.log ('initial createPinia: ' + typeof createPinia)
+    console.log ('initial piniaPersist: ' + typeof piniaPersist)
+
+  return import('pinia')
+  	.then (({ createPinia }) => {
+      console.log ('then createPinia: ' + typeof createPinia)
+        // const { default:piniaPersist } = import ('pinia-plugin-persist')
+        // const piniaPersist = import ('pinia-plugin-persist')
+        return [ createPinia, import ('pinia-plugin-persist') ]
+        // return [ createPinia, piniaPersist ]
+  	})
+    .then (([ createPinia, piniaPersist ]) => {
+        console.log ('then2 createPinia: ' + typeof createPinia +
+            ', piniaPersist: ' + typeof piniaPersist)
+        console.log('then2 piniaPersist-strg: ' + JSON.stringify(piniaPersist))
+        console.log('then2 piniaPersist: ' + piniaPersist)
+
+  // return Promise.all ([ createPinia, piniaPersist ])
+  //   .then ((values) => {
+  //       console.log (' values: '+ JSON.stringify(values))
+  //       const [ createPinia, piniaPersist ] = values
+
+
+        const pinia = createPinia()
+        pinia.use(piniaPersist)
+        console.log('PINIA CREATED: ' + JSON.stringify(pinia))
+
+        console.log ('INSTALLING PlUGIN: ' + JSON.stringify(pinia))
+        app.use(pinia)
+        console.log ('PINIA INSTALLED: ' + pinia) // becomes circular
+    })
+    .catch ((err) => {
+        console.log ('in addPlugins:err: ' + err)
+    })
 }
+
+// preparePinia() // try first here
 
 // https://astro.build/config
 export default defineConfig({
