144e3bd126353514156fbaa081e33de044134fff
nsd <narrationsd@gmail.com>
2022-07-30 02:48:07 -0700

    style fit, begin colors

--- src/components/MdThree.vue
+++ src/components/MdThree.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
     <NoLibs />
-    <br>
-    <Mkdown :source="source" breaks />
-    <br>
-<!--  </div>
-  <div>-->
-    <h2>Last, we have a date, pinia persisted, also per page:</h2>
-    <p>date in store: {{dateStore.date3}}</p>
+    <div class="espace">
+      <Mkdown :source="source" breaks />
+    </div>
+    <div class="espace">
+      <h2>Last, we have a date, pinia persisted, also per page:</h2>
+      <p>date in store: {{ dateStore.date3 }}</p>
+    </div>
     <Datepicker v-model="dateStore.date3" />
     <h4>...and we're done</h4>
   </div>
 </template>
+
+<style scoped>
+.espace {
+  margin: 10px 0;
+  color: d
+}
+</style>
 
 <script setup>
 import Mkdown from 'vue3-markdown-it'
--- src/components/NoLibs.vue
+++ src/components/NoLibs.vue
@@ -14,5 +14,7 @@ export default {
 </script>
 
 <style scoped>
-
+h3 {
+  color: darkblue !important;
+}
 </style>
