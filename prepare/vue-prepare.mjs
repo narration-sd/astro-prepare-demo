72e5cf6673af63ec4b659e2ebd3830031e8b1e46
nsd <narrationsd@gmail.com>
2022-07-31 15:25:48 -0700

    ok, this proves what's though out after earlier needs are closed, is correct. We only need the import() form in the integration side. Now prepare is recast with fixed imports, and still to use promises throughout, while considering if there are any advantages there for startup multitask. Interestingly, this would be entirely optional, leaving open simple try-catch-rethrow nesting for some to use. Notes are brought to present, including comment on the Turbo situation.

--- prepare/vue-prepare.mjs
+++ prepare/vue-prepare.mjs
@@ -1,34 +1,48 @@
-// integration prepares should be mjs, and will be found if present
-// in the modules folder. ext-vue-prepare.mjs is the one that the Astro
-// vue integration would use. Don't forget install the libraries these
-// prepares use into the Astro app's node_modules.
+// integration prepares must be mjs, so that they can be loaded by ES-modules,
+// which is the form used by Astro and its reliance mainly on Vite and browser
+// abilities.
+
+// Prepares will be found if present in the prepare folder. named in the pattern
+//   integrationname-prepare.mjs
 //
-// prepares are expected to run after an integration's basis exists,
-// but before it is mounted, so that plugins etc. can be added.
+//   For example,  vue-prepare.mjs
 
-// some routines to be run, each with some specialties
+// prepares do the creation of the app, so that they can add plugins to it,
+// as for vue.use(). Of course, plugins may have their own enhancements,
+// shown with the pinia.use() here.
 
-// *todo* really, this ought to go in a very simple hotwiredTurbo integration...
-// Our first addtion is Hotwired Turbo, so that we get SPA speed pages,
-// or could add animations. Just importing Turbo fires it off; if it's not
-// tolerant unless in browserland.  Wee do have to await the import(),
-// however, without await. As well, Turbo doesn't prefer to be reinitialized
-// rapidly, while Astro will try it, so we protect against that, as well.
+// At present, we're doing @hotwired/turbo setup as well, but this deserves
+// to be in its own integration, and will be moved there to supplant the original,
+// as this appears to work much more successfully and is on a road to continue
+// in its support. Questions there if all of that @hotwired platform ought to be
+// in one integration -- this would be cleaner, and allow tracking its coming
+// capability releases.
+
 import {createApp} from "vue";
+import { createPinia } from 'pinia'
+import piniaPersist from 'pinia-plugin-persist'
 
 const prepareTurbo = function () {
-    if (typeof window !== 'undefined'
-        && typeof window.Turbo === 'undefined') {
-        return import ('@hotwired/turbo')
-            .then (() => {
-                console.log ('@hotwired/turbo installed')
-                return true
-            })
-            .catch(err =>  {
-                console.log('import(@hotwired/turbo:failed: ' + err)
-                return false
-            })
-    }
+    return new Promise ((resolve, reject) => {
+
+        // we don't want to multiple-create, per component or page...
+        if (typeof window !== 'undefined'
+            && typeof window.Turbo === 'undefined') {
+            return import ('@hotwired/turbo')
+                .then (() => {
+                    console.log ('@hotwired/turbo installed')
+                    return resolve()
+                })
+                .catch(err =>  {
+                    const reason = 'import(@hotwired/turbo:failed: ' + err
+                    console.log(reason)
+                    reject(reason)
+                })
+        }
+        else {
+            resolve()
+        }
+    })
 }
 
 // Pinia needs to be use()ed as a plugin by the Vue initial app,
@@ -37,73 +51,23 @@ const prepareTurbo = function () {
 // taken care of is that running in Astro dev somehow upsets
 // Turbo enough that though it's there, it simply doesn't run.
 const preparePinia = function () {
-    let piniaCreator
-    return import('pinia')
-        .then (({ createPinia }) => {
-            console.log ('then createPinia: ' + typeof createPinia)
-            // const { default:piniaPersist } = import ('pinia-plugin-persist')
-            // const piniaPersist = import ('pinia-plugin-persist')
-            // *todo* messy but necessary; see note just below on array, object altternative
-            piniaCreator = createPinia
-            return import ('pinia-plugin-persist')
-            //
-            // // *todo* nota very bene: this will not work, doesn't cause Promise wait
-            // //  *todo* on import(). Doing this way passing an object instead of array makes
-            // //  *todo* no difference, same problem
-            // // return [ import ('pinia-plugin-persist'), createPinia ]
-            // return [ import ('pinia-plugin-persist'), createPinia ]
-            // // return [ createPinia, piniaPersist ]
-        })
-        .catch (err => {
-            console.log('import(pinia-plugin-persist):failed: ' + err)
-            return false
-        })
-        .then ((piniaPersist) => {
-            const createPinia = piniaCreator
-            // .then (([ piniaPersist, createPinia ]) => {
 
-            console.log('then2 typeof createPinia: ' + typeof createPinia)
-            console.dir(createPinia)
-            console.log('then2 typeof piniaPersist: ' + typeof piniaPersist)
-            console.log('then2 typeof piniaPersist.default: ' + typeof piniaPersist.default)
-            // console.log('then2 piniaPersist-strg: ' + JSON.stringify(piniaPersist))
-            console.dir(piniaPersist)
-            console.dir(piniaPersist.default)
-            // console.log('then2 piniaPersist: ' + piniaPersist)
+        return new Promise ((resolve, reject) => {
+                try {
+                    const pinia = createPinia()
+                    console.log('PINIA INSTALLING persist: ' + JSON.stringify(piniaPersist))
+                    // pinia.use(piniaPersist.default)
+                    pinia.use(piniaPersist)
+                    console.log('PINIA CREATED: ' + JSON.stringify(pinia))
+                    resolve(pinia)
+                } catch (err) {
+                    const reason = 'pinia with pinia-plugin-persist failed: ' + err
+                    console.error(reason)
+                    reject(reason)
+                }
+            }
+        )
 
-            // *todo* notes here for explaining how Promise.all does not work
-            // and that there are other advantages to the split: that we can
-            // individual catch a failed import, and also log to console natures of retunns
-            // that this whole thing is a systems-level exercise, so shoudl have
-            // the thougthfulness and coverage of error possibilities, not to say
-            // discovery for unexpected cases during development or fixing for library
-            // changes causing reversions.
-
-            // now, the real problem:
-            // something about the nature of import()ed wrong lib types
-            // so that piniaPersist.default doesn't work in that case
-            // const createPinia = import('pinia')
-            // const piniaPersist = import ('pinia-plugin-persist')
-            // return Promise.all ([createPinia, piniaPersist])
-            //     .then (([ createPinia, piniaPersist ]) => {
-            //
-            // a checking
-            // return Promise.all ([ createPinia, piniaPersist ])
-            //   .then ((values) => {
-            //       console.log (' values: '+ JSON.stringify(values))
-            //       const [ createPinia, piniaPersist ] = values
-
-
-            const pinia = createPinia()
-            console.log('PINIA INSTALLING persist: ' + JSON.stringify(piniaPersist))
-            pinia.use(piniaPersist.default)
-            console.log('PINIA CREATED: ' + JSON.stringify(pinia))
-            return pinia
-        })
-        .catch (err => {
-            console.error('pinia.use pinia-plugin-persist failed: ' + err)
-            return null
-        })
 }
 
 // this version creates and returns the app, once the essentials are in place for it
@@ -111,18 +75,10 @@ const prepare = function (name = 'not named', createArgs) {
 
     console.log ('About to prepare for: ' + name)
 
-    prepareTurbo() // *todo* move back to chain, check chain intermediate catch semantics, use re-throws to unify
-
-    return preparePinia()
-        // .then (result => {
-        //     return result
-        // })
-        // .then (() => {
-        //     return prepareTurbo()
-        // })
+    return prepareTurbo()
+        .then (() => preparePinia())
         .then (pinia => {
             console.log ('prepare creating app for: ' + name)
-
             const { h, Component, props, slots} = createArgs
             const app = createApp({ name, render: () => h(Component, props, slots) })
             console.log ('crsated app for: ' + name)
