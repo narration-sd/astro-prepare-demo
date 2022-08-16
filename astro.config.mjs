bae03d414e3c9078cb71a8799aa5ce3162a4162b
nsd <narrationsd@gmail.com>
2022-08-16 16:45:06 -0700

    now operating with astro 1.0.6. Surprisingly having to recast NsdMarkdown.vue to create everything in the setup script. Can the Astro change from 1.0.3 have to do also with the need to pin @vuepic/vue-datepicker?

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,65 +2,6 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import vuetify from 'vite-plugin-vuetify'
 
-// https://astro.build/config
-
-// export default defineConfig({
-//   integrations: [vue(),
-//     {
-//       name: 'namespace:id',
-//       hooks: {
-//         'astro:config:done': () => {
-//           console.log('running DONE config')
-//           return {
-//             command: 'dev' | 'build',
-//             updateConfig: {
-//               vite: {
-//                 plugins: [vitePluginVuetify({autoImport: true})],
-//               }
-//             }
-//           }
-//         }
-//       }
-//     },
-//   ],
-// });
-
-function myIntegration() {
-  return {
-    name: 'vueify-after-vue',
-    hooks: {
-      'astro:config:start': () => {
-        console.log('running START config')
-        return {
-          command: 'dev' | 'build',
-          config: {
-            vite: {
-              plugins: [vue(), vuetify({autoImport: true})],
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-// function getViteConfiguration() {
-//   return {
-//     optimizeDeps: {
-//       include: [
-//         '@astrojs/lit/client-shim.js',
-//         '@astrojs/lit/hydration-support.js',
-//         '@webcomponents/template-shadowroot/template-shadowroot.js',
-//         'lit/experimental-hydrate-support.js',
-//       ],
-//       exclude: ['@astrojs/lit/server.js'],
-//     },
-//     ssr: {
-//       external: ['lit-element', '@lit-labs/ssr', '@astrojs/lit'],
-//     },
-//   };
-// }
-
 // note that in both uses, this works because arrays are passed by reference in JS
 // which puts a kind of funny cast on updateConfig, but where usable, that's clearer,
 // while this is careful, and very safe.
@@ -88,21 +29,8 @@ function vuetifyIntegration (options) {
           // though none of this helps the dev with ssr problem, so far
           console.log('dev, and is there ssr? : ' + JSON.stringify(config.vite.ssr))
           updateConfig ({
-            // vite: {
-            //   ssr: {
-            //     noExternal: [
-            //         'vuetify'
-            //     ]
-            //   },
-            //   // template: {
-            //   //   compilerOptions: {
-            //   //     isCustomElement: tag => tag.startsWith('v-') // vuetify web components
-            //   //   }
-            //   // }
-            // },
             // *todo* if we're going to do this, pass in vue and vuetify options...tbd
             plugins: [vue(), vuetify({autoImport: true})],
-            // transpile: ['vuetify', 'NsdMicroLogo.vue'],
           })
           clearVuetify(config.vite)
         }
@@ -110,11 +38,9 @@ function vuetifyIntegration (options) {
       'astro:build:setup': ({ vite, target, updateConfig }) => {
         console.log('astro.config:VITE:TARGET: ' + target)
         updateConfig ({
+          // *todo* same on args as above...
           plugins: [ vue(), vuetify({autoImport: true}) ],
-          // transpile: ['vuetify', 'NsdMicroLogo.vue'],
         })
-        // *todo* not needed, but left to indicate how wwe can hit rollupOptions
-        // vite.build.rollupOptions.external = [ "vuetify/components" ]
         console.log ('VITE.build.setup: ' + JSON.stringify(vite.build))
         console.log('VITE.build.setup.target: ' + target)
         if (target === 'server') {
@@ -127,5 +53,4 @@ function vuetifyIntegration (options) {
 
 export default defineConfig({
   integrations: [vue(), vuetifyIntegration()]
-  // integrations: [vue()]
 })
--- src/components/NsdMarkdown.vue
+++ src/components/NsdMarkdown.vue
@@ -1,26 +1,24 @@
 <template>
+<!--  this nasty thing you do _only_ if you can be sure what's passed in prop is safe... -->
 <div v-html="formattedMd"></div>
 </template>
 
 <script setup>
+import { defineProps, computed } from 'vue'
 import MarkdownIt from "markdown-it";
+
 const md = new MarkdownIt()
+const props = defineProps({
+  source: { type: String, default: '# NsdMarkdown,\n   ***at your service***'}
+})
+
+const formattedMd = computed (() => {
+    return md.render(props.source)
+  })
 </script>
 
 <script>
 export default {
-  props: {
-    source: { type: String, default: '# NsdMarkdown,\n   ***at your service***'}
-  },
-  computed: {
-    formattedMd: function () {
-      return this.md.render(this.source)
-    }
-  },
   name: "NsdMarkdown"
 }
 </script>
-
-<style scoped>
-
-</style>
