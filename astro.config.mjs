8cf580e135f92635ce43129f67471318ac390185
Narration SD <narration-sd@users.noreply.github.com>
2022-10-19 13:04:00 -0700

    Ok, transfer to Nate's access of tracked virtual module just works, no hitches. A bit of clarifying on the astro-config to go with.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,21 @@ import vue from '@astrojs/vue';
 import { vuetifyIntegration } from "./prepare/vue-vuetify-integration.mjs";
 import { piniaIntegration } from "./prepare/vue-pinia-integration.mjs";
 import {addBuildErrorReportingCase} from "./prepare/prepare-utils.js";
+// (see below as to why commented out, as this does work of course
 // import tailwind from '@astrojs/tailwind';
 
-
-addBuildErrorReportingCase( /\-prepare/, 'how about a message')
-
 export default defineConfig({
+
   // n.b. Order must be followed here: each platform integration first, then any related integrations
-  // This is so the added integration operates against its platform already configured
-  // In this published example, vue(), then vuetifyIntegration()...
-  integrations: [ vue(), vuetifyIntegration(), piniaIntegration()/*, tailwind(), */  ]
+  // This is so each added integration operates against its platform already configured
+  // In this published example, vue() with its option/s, then vuetifyIntegration()...
+
+  integrations: [
+      vue({
+    appEntrypoint: '/prepare/vue-prepare', // cleaner with own folder, for more integrations and support modules
+  }),
+    piniaIntegration(),
+    vuetifyIntegration(),
+    /*, tailwind(), out because it crushes css Vuetify and others use - it's a one-frame framer */
+  ]
 })
